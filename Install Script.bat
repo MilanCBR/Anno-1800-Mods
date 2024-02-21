2365783b2d985d594e31c0f6895e19d44c270900
MilanCBR <milangeleynse@gmail.com>
2024-02-21 12:43:17 +0100

    Added Install Script

---
+++ Install Script.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: Define variables
+set githubUrl=https://github.com/MilanCBR/Anno_Mods
+set destination=C:\Users\Public\mod.io\4169\mods
+set tempZipFile=C:\Users\Public\mod.io\4169\mods\Anno_Mods.zip
+
+:: Create the destination folder if it doesn't exist
+if not exist "%destination%" mkdir "%destination%"
+
+:: Download the repository as a zip file
+powershell -Command "Invoke-WebRequest -Uri '%githubUrl%/archive/refs/heads/main.zip' -OutFile '%tempZipFile%'"
+
+:: Unzip the downloaded file directly to the destination
+powershell -Command "Expand-Archive -Path '%tempZipFile%' -DestinationPath '%destination%' -Force"
+
+:: Move the contents of the Anno_Mods-main folder to the destination folder
+powershell -Command "Get-ChildItem -Path '%destination%\Anno_Mods-main' -Recurse | Move-Item -Destination '%destination%'"
+
+:: Remove the Anno_Mods-main folder
+powershell -Command "Remove-Item -Path '%destination%\Anno_Mods-main' -Recurse -Force"
+
+:: Clean up the zip file
+del "%tempZipFile%"
+
+echo Folders downloaded and placed in %destination%
+pause
+
+
